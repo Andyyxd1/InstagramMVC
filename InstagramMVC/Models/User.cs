170b72975e7bfa827e6c95d10fec9c4b8bc66e5f
mikailb <115622678+mikailb@users.noreply.github.com>
2024-09-24 13:05:03 +0200

    komma
    fjernet komma

--- InstagramMVC/Models/User.cs
+++ InstagramMVC/Models/User.cs
@@ -4,8 +4,8 @@ namespace InstagramMVC.Models;
 
 public class User
 {
-    public int UserId {get; set;},
-    public string Name {get; set;},
-    public string Email {get; set;},
+    public int UserId {get; set;}
+    public string Name {get; set;}
+    public string Email {get; set;}
     public string Password {get; set;}
 }
